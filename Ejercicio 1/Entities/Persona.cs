8f022731d2a5490cab9a559ebec3fc3be491b008
TOLLAN13 <aqrbernardo210203@gmail.com>
2023-06-13 12:39:48 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio 1/Entities/Persona.cs
@@ -0,0 +1,47 @@
+﻿using Ejercicio_1.Funciones;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1.Clases
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set;}
+        public string Carrera { get; set; }
+        public int Edad { get; set;}
+
+        public void PedirDatos()
+        {
+            Console.WriteLine("Ingresa tu nombre");
+            this.Nombre = Console.ReadLine();
+            Console.WriteLine("Ingresa tu Apellido");
+            this.Apellido = Console.ReadLine();
+            Console.WriteLine("Ingresa tu Carrera");
+            this.Carrera = Console.ReadLine();
+            Console.WriteLine("Ingresa tu Edad");
+            this.Edad = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            if (Edad >= 18)
+            {
+                Menu menu= new Menu();
+
+                menu.Opciones();
+            }
+            else
+            {
+                Console.WriteLine("No puedes acceder si no eres mayor");
+            }
+        }
+
+        
+    }
+
+    
+}
+
